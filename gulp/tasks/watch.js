6b9f53008d588cffd9270fdc5e8f05700814e9f1
tvollmer89 <tvollmer89@gmail.com>
2018-11-13 14:23:54 -0600

    Completed destop and mobile header styles

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,7 +3,6 @@ watch = require('gulp-watch'),
 browserSync = require('browser-sync').create();
 
 
-
 gulp.task('watch', function() {
 
   browserSync.init({
@@ -21,9 +20,17 @@ gulp.task('watch', function() {
     gulp.start('cssInject');
   });
 
+  watch('./app/assets/scripts/**/*.js', function () {
+    gulp.start('scriptsRefresh');
+  });
+
 });
 
 gulp.task('cssInject', ['styles'], function() {
   return gulp.src('./app/temp/styles/styles.css')
     .pipe(browserSync.stream());
 });
+
+gulp.task('scriptsRefresh', ['scripts'], function() {
+  browserSync.reload();
+})
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: "./app/assets/scripts/app.js",
+  output: {
+    path: path.resolve(__dirname, "./app/temp/scripts"),
+    filename: "App.js"
+  },
+module: {
+  rules: [
+    {
+      test: /\.m?js$/,
+      exclude: /(node_modules|bower_components)/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env']
+        }
+      }
+    }
+  ]
+}
+}
